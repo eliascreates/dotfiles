88978feb646a51cd3bb887ef9a7ed00f06ccc23a
Elias Kekana <thapeloelias.kekana@gmail.com>
2025-04-20 12:32:10 +0200

    update Windows Terminal, Profile and settings.json

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,10 @@
-oh-my-posh.exe init pwsh --config "C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\atomic.omp.json" | Invoke-Expression
+$customTheme = "C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\custom_atomic.omp.json"
+$fallbackTheme = "C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\robbyrussell.omp.json"
+
+# Use fallback if custom theme does not exists
+$themeToUse = if (Test-Path $customTheme) { $customTheme } else { $fallbackTheme }
+
+oh-my-posh.exe init pwsh --config $themeToUse | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 Import-Module posh-git

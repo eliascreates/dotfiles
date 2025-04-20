5e7d67d246aa04537881a1d3a1f4ae13b7b679b5
Elias Kekana <thapeloelias.kekana@gmail.com>
2025-04-20 12:44:46 +0200

    fix powershell PROFILE

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
-$customTheme = "C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\custom_atomic.omp.json"
-$fallbackTheme = "C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\robbyrussell.omp.json"
+$customTheme = "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\custom_atomic.omp.json"
+$fallbackTheme = "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\robbyrussell.omp.json"
 
-# Use fallback if custom theme does not exists
+# Use fallback if custom theme does not exist
 $themeToUse = if (Test-Path $customTheme) { $customTheme } else { $fallbackTheme }
 
-oh-my-posh.exe init pwsh --config $themeToUse | Invoke-Expression
+oh-my-posh init pwsh --config $themeToUse | Invoke-Expression
 
-Import-Module -Name Terminal-Icons
-Import-Module posh-git
+Import-Module -Name Terminal-Icons # -ErrorAction SilentlyContinue
+Import-Module -Name posh-git # -ErrorAction SilentlyContinue
